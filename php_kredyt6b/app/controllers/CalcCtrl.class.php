073c23afd6fcf98f68eb2313611b4c7310c63c86
Natu125 <163070850+Natu125@users.noreply.github.com>
2024-04-15 22:37:21 +0200

    Add files via upload

---
+++ php_kredyt6b/app/controllers/CalcCtrl.class.php
@@ -0,0 +1,148 @@
+<?php
+namespace app\controllers;
+
+//zamieniamy zatem 'require' na 'use' wskazując jedynie przestrzeń nazw, w której znajduje się klasa
+use app\forms\CalcForm;
+use app\transfer\CalcResult;
+
+/** Kontroler kalkulatora
+
+ *
+ */
+class CalcCtrl {
+
+	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $result; //inne dane dla widoku
+	//private $hide_intro; //zmienna informująca o tym czy schować intro
+
+	/** 
+	 * Konstruktor - inicjalizacja właściwości
+	 */
+	public function __construct(){
+		//stworzenie potrzebnych obiektów
+		$this->form = new CalcForm();
+		$this->result = new CalcResult();
+		//$this->hide_intro = false;
+	}
+	
+	/** 
+	 * Pobranie parametrów
+	 */
+	public function getParams(){
+		//$this->form->x = isset($_REQUEST ['x']) ? $_REQUEST ['x'] : null;
+		//$this->form->y = isset($_REQUEST ['y']) ? $_REQUEST ['y'] : null;
+		//$this->form->op = isset($_REQUEST ['op']) ? $_REQUEST ['op'] : null;
+
+		$this->form->kwota = getFromRequest('kwota');
+		$this->form->lat = getFromRequest('lat');
+		$this->form->opr = getFromRequest('opr');
+	}
+	
+	/** 
+	 * Walidacja parametrów
+	 * @return true jeśli brak błedów, false w przeciwnym wypadku 
+	 */
+	public function validate() {
+		// sprawdzenie, czy parametry zostały przekazane
+		if (! (isset ( $this->form->kwota ) && isset ( $this->form->lat ) && isset ( $this->form->opr ))) {
+			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+			return false;
+		}// else { 
+			//$this->hide_intro = true; //przyszły pola formularza - schowaj wstęp
+		//}
+		
+		// sprawdzenie, czy potrzebne wartości zostały przekazane
+		if ($this->form->kwota == "") {
+			getMessages()->addError('Nie podano kwoty');
+		}
+		if ($this->form->lat == "") {
+			getMessages()->addError('Nie podano lat');
+		}
+		if ($this->form->opr == "") {
+			getMessages()->addError('Nie podano oprocentowania');
+		}
+		
+		// nie ma sensu walidować dalej gdy brak parametrów
+		if (! getMessages()->isError()) {
+			
+			// sprawdzenie, czy $x i $y są liczbami całkowitymi
+			if (! is_numeric ( $this->form->kwota )) {
+				getMessages()->addError('Pierwsza wartość nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->lat )) {
+				getMessages()->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+
+			if (! is_numeric ( $this->form->opr )) {
+				getMessages()->addError('Trzecia wartość nie jest liczbą całkowitą');
+			}
+		}
+		
+		return ! getMessages()->isError();
+	}
+	
+	/** 
+	 * Pobranie wartości, walidacja, obliczenie i wyświetlenie
+	 */
+	public function process(){
+
+		$this->getparams();
+		
+		if ($this->validate()) {
+				
+			//konwersja parametrów na int
+			$this->form->kwota = intval($this->form->kwota);
+			$this->form->lat = intval($this->form->lat);
+			$this->form->opr = intval($this->form->opr);
+			getMessages()->addInfo('Parametry poprawne.');
+				
+			//wykonanie operacji
+			/*switch ($this->form->op) {
+				case 'minus' :
+					$this->result->result = $this->form->x - $this->form->y;
+					$this->result->op_name = '-';
+					break;
+				case 'times' :
+					$this->result->result = $this->form->x * $this->form->y;
+					$this->result->op_name = '*';
+					break;
+				case 'div' :
+					$this->result->result = $this->form->x / $this->form->y;
+					$this->result->op_name = '/';
+					break;
+				default :
+					$this->result->result = $this->form->x + $this->form->y;
+					$this->result->op_name = '+';
+					break;
+			}
+			*/
+
+			$this->result->result = $this->form->kwota/($this->form->lat*12)*(1+$this->form->opr/100);
+			
+			getMessages()->addInfo('Wykonano obliczenia.');
+		}
+		
+		$this->generateView();
+	}
+	
+	
+	/**
+	 * Wygenerowanie widoku
+	 */
+	public function generateView(){
+		
+		
+		getSmarty()->assign('page_title','Kalkulator kredytowy');
+		getSmarty()->assign('page_description','Aplikacja z jednym "punktem wejścia". Model MVC, w którym jeden główny kontroler używa różnych obiektów kontrolerów w zależności od wybranej akcji - przekazanej parametrem.');
+		getSmarty()->assign('page_header','Kontroler główny');
+				
+		//$smarty->assign('hide_intro',$this->hide_intro);
+		
+		
+		getSmarty()->assign('form',$this->form);
+		getSmarty()->assign('res',$this->result);
+		
+		getSmarty()->display('CalcView.tpl');
+	}
+}
